9470415d629350d0eaeee4b311ce9518982e83a1
ramigs <ramigs@gmail.com>
2024-11-04 18:39:54 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TeamView from './views/TeamView.vue'
+</script>
+
+<template>
+  <div class="grid m-2 p-2">
+    <div>
+      <TeamView />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
